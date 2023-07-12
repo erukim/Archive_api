bf2622fbc9a82caa59abf296ce96d9f4e1795a46
erukim <erukim@hiplay.kr>
2023-07-13 03:39:48 +0900

    Fix: Test Code

--- test.ts
+++ test.ts
@@ -1,4 +1,12 @@
-import Archive from ".";
+import Archive_Client from ".";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
-const archive = new Archive();
-console.log(archive.test())
+const client = new Client({ intents: [GatewayIntentBits.GuildMessages] })
+client.login(``)
+
+client.on(Events.ClientReady, async () => {
+  if (!client.user) return
+  console.log(client.user.tag)
+  const archive = new Archive_Client('', client.user.id);
+  console.log((await archive.Bot_GetLike(``)))
+})

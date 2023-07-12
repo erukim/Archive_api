85e510b367120baa63863b8cd0c845e0f7341213
erukim <erukim@hiplay.kr>
2023-07-13 00:44:21 +0900

    One Comit Push

---
+++ index.ts
@@ -0,0 +1,7 @@
+export default class Archive {
+    constructor() {
+    }
+    async test() {
+        return true;
+    }
+}
---
+++ test.ts
@@ -0,0 +1,4 @@
+import Archive from ".";
+
+const archive = new Archive();
+console.log(archive.test())
